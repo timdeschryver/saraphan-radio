e4958353419875de6a519d0acb6e42dfc99efcd7
Kate Sky <kateskytech@outlook.com>
2020-04-17 17:07:39 -0500

    fixed prod

--- apps/shell-app/src/environments/environment.prod.ts
+++ apps/shell-app/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true ,
+  authClient:'N9Dp1Zs2ge7cRFRd9QVUoPlnxNg5xi6O',
+  authDomain:'csa-app.auth0.com',
+  path: 'https://katesky.github.io/saraphan-radio',
+  callback: 'https://katesky.github.io/saraphan-radio/callback',
 };
